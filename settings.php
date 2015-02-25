21b2588532b4d6def3e16e0aa497bd84149437c9
Samuel Svensäter <peterodd@gmail.com>
2015-02-25 19:28:50 +0100

    addPurchase and getLastFive works, minor fixes to be done

--- settings.php
+++ settings.php
@@ -24,55 +24,70 @@ function db_connect() {
 }
 
 /* Used to keep track of last five purchases made to the system (this is used by the bartender) */
+/* Make sure to call this function with sizeof($str)%3 == 0 */ 
 function addPurchase() {
 	$db = db_connect();
-	$queryArray = mysqli_real_escape_string($db, $_POST['queryArray']); 
+	$str = $_POST['str']; 
+	//var_dump($str);
+
+	$inputArray = explode(',', $str);
 	$date = new DateTime();
 	$date = $date->format('Y-m-d H:i:s');
 	$numberSql = "SELECT number FROM beers WHERE number=(SELECT max(number) FROM beers)";
-	$number = mysqli_query($db, $numberSql);
+	
+	$result = mysqli_query($db, $numberSql);
+	if (mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_row($result);
+		//printf("row: %d\n", $row[0]);
+		$number = $row[0];
+	}
+	//$number = mysqli_fetch_row($result)
+
+	printf("number: %d\n", $number);
 	if ($number >= 5) { //ev 4
 		$numToDelete = $number-4;
 		$deleteSql = "DELETE FROM beers WHERE number='$numToDelete'";
 		mysqli_query($db, $deleteSql);
 	}
 	$numToAdd = $number+1;
+	//printf("numtoadd: %d\n", $numToAdd);
     
-    foreach ($queryArray as $q) {
-    	$name = $q[0];
-    	$price = $q[1];
-    	$amount = $q[2];
-    	$beerid = $q[3];
-    	$sql = "INSERT INTO beers(number, time, name, price, amount, beer_id) VALUES ('$numToAdd', '$date', '$name', '$price', '$amount', '$beerid')";
-    	$result = mysqli_query($db, $sql);
+	$max = sizeof($inputArray);
+	for ($i=0; $i < $max; $i += 3) { 
+    	$name = $inputArray[$i];
+    	$price = $inputArray[$i+1];
+    	$amount = $inputArray[$i+2];
+    	// printf("name: %s\n", $name);
+    	// printf("name: %s\n", $numToAdd);
+    	// printf("name: %s\n", $date);
+    	// printf("name: %s\n", $price);
+    	// printf("name: %s\n", $amount);
+
+    	$sql = "INSERT INTO beers(number, time, name, price, amount) VALUES ('$numToAdd', '$date', '$name', '$price', '$amount')";
+    	if (mysqli_query($db, $sql)) {
+    		echo "New record created successfully";
+    	} else {
+    		echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    	}
     }
+    mysqli_close($db);
 }
 
 function getLastFive() {
 	$db = db_connect();
 	$resultArray = [];
-	$sql = "SELECT * FROM beers";
+	$sql = "SELECT * FROM beers ORDER BY number";
 	$result = mysqli_query($db, $sql);
 
 	if (mysqli_num_rows($result) > 0) {
     	while ($row = mysqli_fetch_row($result)) {
-    		$tmp = [$row[0], $row[1], $row[2], $row[3], $row[4], $row[5]];
+    		$tmp = [ $row[0], $row[1], $row[2], $row[3], $row[4] ];
     		$resultArray[] = $tmp;
-    		// echo $row[0], PHP_EOL;
-    		// echo $row[1], PHP_EOL;
-    		// echo $row[2], PHP_EOL;
-    		// echo $row[3], PHP_EOL;
-    		// echo $row[4], PHP_EOL;
-    		// echo $row[5], PHP_EOL;
-    		// echo PHP_EOL;
     	}
     }   
     //var_dump($resultArray);
     mysqli_close($db);
-    //echo json_encode($resultArray);
-    //var_dump($resultArray);
-    print json_encode($resultArray);
-    
+    print json_encode($resultArray);   
 }
 
 
