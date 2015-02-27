58ce2bac44262e0b4fa6756865166152ca21011c
ShuoYang777 <ysgood777@gmail.com>
2015-02-28 07:14:35 +0800

    session_final

--- db_login.php
+++ db_login.php
@@ -17,9 +17,10 @@
     }
     return $db;
   }
-Session_Start();
+
     if (isset($_POST['submit'])) {
         //function login() {
+        Session_Start();
         $db = db_connect();
         $username = mysqli_real_escape_string($db, $_POST['username']);
         $password = mysqli_real_escape_string($db, $_POST['password']);
@@ -47,6 +48,15 @@ Session_Start();
                 if ($password != $row[2]) {
                     header("Location:passwordwrong.html");
                 } else if ($row[1] == 0) {
+                    if(isset($_SESSION['expiretime'])) {
+                        if($_SESSION['expiretime'] < time()) {
+                            unset($_SESSION['expiretime']);
+                            header('Location: index.html');
+                            exit(0);
+                        } else {
+                            $_SESSION['expiretime'] = time() + 3600;
+                        }
+                    }
                     $_SESSION['user_id']=$row[0];
                     $_SESSION['credentials']=$row[1];
                     $_SESSION['password']=$row[2];
@@ -72,6 +82,15 @@ Session_Start();
                     mysqli_close($db);
                     exit;
                 } else if ($row[1] == 3) {
+                    if(isset($_SESSION['expiretime'])) {
+                        if($_SESSION['expiretime'] < time()) {
+                            unset($_SESSION['expiretime']);
+                            header('Location: index.html');
+                            exit(0);
+                        } else {
+                            $_SESSION['expiretime'] = time() + 3600;
+                        }
+                    }
                     $_SESSION['user_id']=$row[0];
                     $_SESSION['credentials']=$row[1];
                     $_SESSION['password']=$row[2];
