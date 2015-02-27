080996ee248189f4f8bf453da922c9c28e36bade
ShuoYang777 <ysgood777@gmail.com>
2015-02-27 21:58:46 +0800

    cookie

--- db_login.php
+++ db_login.php
@@ -1,7 +1,7 @@
 <?php 
-  //$func_id = $_POST['func_id'];
+/*$func_id = $_POST['func_id'];
 
-  /*if ($func_id == "0") {
+  if ($func_id == "0") {
     login();
   } elseif ($func_id == "1") {
     createUser();
@@ -17,59 +17,59 @@
     }
     return $db;
   }
-if (isset($_POST['submit'])){
-  //function login() {
-    $db = db_connect();
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-    //echo md5($password);
-    //echo PHP_EOL;
-    $sql = "SELECT * FROM users WHERE username='$username'";
-    $result = mysqli_query($db, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_row($result)) {
-        $password = stripslashes($password);
-        //echo $password, PHP_EOL;
-        $row[2] = stripslashes($row[2]);
-        //echo $row[2], PHP_EOL;
-        $password = md5($password);
-          $row[1] = stripslashes($row[1]);
-          $row[0] = stripslashes($row[0]);
-          $row[3] = stripslashes($row[3]);
-          $row[4] = stripslashes($row[4]);
-          $row[5] = stripslashes($row[5]);
-          $row[6] = stripslashes($row[6]);
-          $row[7] = stripslashes($row[7]);
-          $row[1] = stripslashes($row[8]);
-          $row[1] = stripslashes($row[9]);
-          $hour = time() + 3600;
-          setcookie("user_id", $row[0], $hour);
-          setcookie("credentials", $row[1], $hour);
-          setcookie("password", $row[2], $hour);
-          setcookie("username", $row[3], $hour);
-          setcookie("fname", $row[4], $hour);
-          setcookie("lname", $row[5], $hour);
-          setcookie("email", $row[6], $hour);
-          setcookie("phone", $row[7], $hour);
-          setcookie("credit",$row[8], $hour);
-          setcookie("debt", $row[9], $hour);
+    if (isset($_POST['submit'])) {
+        //function login() {
+        $db = db_connect();
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+        //echo md5($password);
+        //echo PHP_EOL;
+        $sql = "SELECT * FROM users WHERE username='$username'";
+        $result = mysqli_query($db, $sql);
 
-        if ($password != $row[2]) {
-          echo "Wrong password";
-          //TODO: Ask the user to enter credentials again
-        } else if($row[1]== 0){
-            header("Location:bartender.html");
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_row($result)) {
+                $password = stripslashes($password);
+                //echo $password, PHP_EOL;
+                $row[2] = stripslashes($row[2]);
+                //echo $row[2], PHP_EOL;
+                $password = md5($password);
+                $row[1] = stripslashes($row[1]);
+                $row[0] = stripslashes($row[0]);
+                $row[3] = stripslashes($row[3]);
+                $row[4] = stripslashes($row[4]);
+                $row[5] = stripslashes($row[5]);
+                $row[6] = stripslashes($row[6]);
+                $row[7] = stripslashes($row[7]);
+                $row[1] = stripslashes($row[8]);
+                $row[1] = stripslashes($row[9]);
+                $hour = time() + 3600;
+                setcookie("user_id", $row[0], $hour);
+                setcookie("credentials", $row[1], $hour);
+                setcookie("password", $row[2], $hour);
+                setcookie("username", $row[3], $hour);
+                setcookie("fname", $row[4], $hour);
+                setcookie("lname", $row[5], $hour);
+                setcookie("email", $row[6], $hour);
+                setcookie("phone", $row[7], $hour);
+                setcookie("credit", $row[8], $hour);
+                setcookie("debt", $row[9], $hour);
+
+                if ($password != $row[2]) {
+
+                    header("Location:index.html");
+                } else if ($row[1] == 0) {
+                    header("Location:bartender.html");
+                } else if ($row[1] == 3) {
+                    header("Location:vip.html");
+                }
+            }
+        } else {
+            echo "0 results";
+
         }
-          else if ($row[1]== 3)
-          {
-              header("Location:vip.html");
-          }
-      }
-    } else {
-        echo "0 results";
-        //mysqli_close($db);
-    }
+        mysqli_close($db);
     }
 
     /* create a new user to this database if a new user is created in the API */
@@ -80,8 +80,7 @@ if (isset($_POST['submit'])){
       $firstname = mysqli_real_escape_string($db, $_POST['firstname']);
       $lastname = mysqli_real_escape_string($db, $_POST['lastname']);
       $email = mysqli_real_escape_string($db, $_POST['email']);
-      $phone = mysqli_real_escape_string($db, $_POST['phone']); 
-
+      $phone = mysqli_real_escape_string($db, $_POST['phone']);
       $password = md5($pword);
       $sql = "INSERT INTO users(credentials, password, username, first_name, last_name, email, phone, credit, debt) VALUES (3,'$password','$uname','$firstname','$lastname','$email','$phone',0,0)";
       if (mysqli_query($db, $sql)) {
