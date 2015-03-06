66537e435291b3d6a892b371b9f36d4c36b2d62f
Mohammad Nurul Azim Bhuiyan <azim190590@gmail.com>
2015-03-06 18:21:25 +0100

    Conflicts

--- js/translate.js
+++ js/translate.js
@@ -6,8 +6,11 @@ i18n.init({lng: window.language } , function(t) {
   $(".button").i18n();
   $(".menuBtn").i18n();
     $(".orderbutton").i18n();
+<<<<<<< HEAD
     $(".content").i18n();
     // document.getElementById('iframeID').contentWindow.document.getElementById('idinsideiframe')
+=======
+>>>>>>> parent of b009be1... VIP beer buy now works
 
   // programatical access
   var appName = t("index.title");
