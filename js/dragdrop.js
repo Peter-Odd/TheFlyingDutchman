d625386dc1dd862395a70342f5eeac92790173c4
Samuel Svensäter <peterodd@gmail.com>
2015-03-10 16:06:50 +0100

    added rating function so vip can rate beer using drag n drop

---
+++ js/dragdrop.js
@@ -0,0 +1,17 @@
+/* Functions for rating beers using drag n drop */
+function dragBeerRating(ev) {
+	ev.target.style.opacity = "0.3";
+}
+
+function dragEndRating(t) {
+	t.draggable = false;
+}
+
+function dropRating(ev) {
+	ev.preventDefault();
+	var beer = ev.toElement.alt;
+	if (sessionStorage.length == 0) { createInventory() }
+	var tmp = JSON.parse(sessionStorage[beer.toLowerCase()]);
+	tmp[3] = 1;
+	sessionStorage[beer.toLowerCase()] = JSON.stringify(tmp);				
+}
